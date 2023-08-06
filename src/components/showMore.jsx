01ea8542262a2249bbeab49421c8b4586fff11d4
Danny <dannyjmargolin@gmail.com>
2023-08-05 21:30:47 -0500

    updated element dance desc

--- src/components/showMore.jsx
+++ src/components/showMore.jsx
@@ -9,13 +9,14 @@ const ShowMore = ({ text, maxCharacters }) => {
   };
 
   const displayText = showMore ? text : text.substr(0, maxCharacters).trim();
+  const displayEllipsis = !showMore && text.length > maxCharacters ? "..." : "";
 
   return (
     <div className={styles.textContainer}>
-      <p className={styles.bg}>
-        {displayText}
-        {!showMore && text.length > maxCharacters && "..."}
-      </p>
+      <p
+        className={styles.bg}
+        dangerouslySetInnerHTML={{ __html: displayText + displayEllipsis }}
+      />
       {text.length > maxCharacters && (
         <button className={styles.showMoreButton} onClick={toggleShowMore}>
           {showMore ? "Show Less" : "Show More"}
